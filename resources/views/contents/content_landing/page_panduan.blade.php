2445f7223f4595d335b6a79d3f0b8161763f8c39
Ariston <zacedev@gmail.com>
2024-10-03 10:44:38 +0700

    Update

--- resources/views/contents/content_landing/page_panduan.blade.php
+++ resources/views/contents/content_landing/page_panduan.blade.php
@@ -10,7 +10,7 @@ Lab management | Dashboard
       <div class="container">
         <div class="row d-flex justify-content-center text-center">
           <div class="col-lg-8">
-            <h1>Panduan Registrasi</h1>
+            <h1>Panduan Penggunaan</h1>
             {{-- <p class="mb-0">Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.</p> --}}
           </div>
         </div>
@@ -34,13 +34,12 @@ Lab management | Dashboard
           <div class="course-item">
             {{-- <img src="assets/img/course-1.jpg" class="img-fluid" alt="..."> --}}
             <video id="player1" playsinline controls>
-              <source src="{{ url('/public/assets/video/panduan_1_umum.mp4') }}" type="video/mp4" />
-              <track kind="captions" label="English captions" src="MY_CAPTIONS.vtt" srclang="en" default />
+              <source src="{{ url('/public/assets/video/mahasiswa_pinjam_lab.mp4') }}" type="video/mp4" />
+              {{-- <track kind="captions" label="English captions" src="MY_CAPTIONS.vtt" srclang="en" default /> --}}
             </video>
             <div class="course-content mt-3" style="width: 100%">
               <div class="d-flex justify-content-between align-items-center mb-3">
-                <p class="category">Web Development</p>
-                <p class="price">$169</p>
+                <p class="category">Panduan Peminjaman Lab. Untuk Mahasiswa</p>
               </div>
             </div>
           </div>
@@ -49,52 +48,130 @@ Lab management | Dashboard
           <div class="course-item">
             {{-- <img src="assets/img/course-1.jpg" class="img-fluid" alt="..."> --}}
             <video id="player2" playsinline controls>
-              <source src="{{ url('/public/assets/video/panduan_2_student.mp4') }}" type="video/mp4" />
-              <track kind="captions" label="English captions" src="MY_CAPTIONS.vtt" srclang="en" default />
+              <source src="{{ url('/public/assets/video/mahasiswa_sewa_alat.mp4') }}" type="video/mp4" />
+              {{-- <track kind="captions" label="English captions" src="MY_CAPTIONS.vtt" srclang="en" default /> --}}
             </video>
             <div class="course-content mt-3" style="width: 100%">
               <div class="d-flex justify-content-between align-items-center mb-3">
-                <p class="category">Web Development</p>
-                <p class="price">$169</p>
+                <p class="category">Panduan Penyewaan Alat Lab. Untuk Mahasiswa</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+          <div class="course-item">
+            {{-- <img src="assets/img/course-1.jpg" class="img-fluid" alt="..."> --}}
+            <video id="player3" playsinline controls>
+              <source src="{{ url('/public/assets/video/mahasiswa_uji_lab.mp4') }}" type="video/mp4" />
+              {{-- <track kind="captions" label="English captions" src="MY_CAPTIONS.vtt" srclang="en" default /> --}}
+            </video>
+            <div class="course-content mt-3" style="width: 100%">
+              <div class="d-flex justify-content-between align-items-center mb-3">
+                <p class="category">Panduan Uji Laboratorium Untuk Mahasiswa</p>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="row gy-4">
-        <div class="col-md-12 order-1 order-lg-2" style="color: aliceblue">
-          <div class="mb-3">
+      <div class="row">
+        <div class="col-12" style="text-align: center;margin-bottom: 20px;">
+          <h3>Panduan Untuk Dosen</h3>
+        </div>
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+          <div class="course-item">
+            {{-- <img src="assets/img/course-1.jpg" class="img-fluid" alt="..."> --}}
+            <video id="player4" playsinline controls>
+              <source src="{{ url('/public/assets/video/dosen_pinjam_lab.mp4') }}" type="video/mp4" />
+              {{-- <track kind="captions" label="English captions" src="MY_CAPTIONS.vtt" srclang="en" default /> --}}
+            </video>
+            <div class="course-content mt-3" style="width: 100%">
+              <div class="d-flex justify-content-between align-items-center mb-3">
+                <p class="category">Panduan Pengajuan Peminjaman Lab. Untuk Dosen</p>
+              </div>
+            </div>
           </div>
-          <ol type="A">
-            <li style="margin-bottom: 30px;"> <b>Panduan Pengguna User Umum</b> <br>
-             
-            </li>
-            <li style="margin-bottom: 30px;">
-              <b>Panduan Pengguna User Mahasiswa FT</b> <br>
-              
-            </li>
-            <li>
-              Download Panduan PDF. <a href="{{ url('/public/assets/img/panduan.pdf') }}"> <label class="label label-default">Panduan.pdf</label></a>
-            </li>
-          </ol>
-          {{-- <div class="pb-4" style="text-align: center;">
-            <img src="{{ url("/public/assets/img/Panduan_ft.jpg") }}" style="max-width: 900px;" alt="">
-          </div> --}}
-          {{-- <b>Panduan Registrasi</b> <br>
-          <ol>
-            <li>Klik tombol login pada pojok kanan atas</li>
-            <li>
-              Pilih menu link Daftar disini <br>
-              <img src="{{ url("/public/assets/img/reg_menu.PNG") }}" style="max-width: 300px;" alt=""> <br> <br>
-            </li>
-            <li>
-              Isi Form registrasi yang tersedia, harap isikan sesuai dengan status anda. <br>
-              <img src="{{ url("/public/assets/img/reg_menu_2.PNG") }}" style="max-width: 300px;" alt=""> <br> <br>
-            </li>
-            <li>
-              Selanjutnya klik simpan, dan registrasi selesai.
-            </li>
-          </ol> --}}
+        </div>
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+          <div class="course-item">
+            {{-- <img src="assets/img/course-1.jpg" class="img-fluid" alt="..."> --}}
+            <video id="player5" playsinline controls>
+              <source src="{{ url('/public/assets/video/dosen_sewa_alat.mp4') }}" type="video/mp4" />
+              {{-- <track kind="captions" label="English captions" src="MY_CAPTIONS.vtt" srclang="en" default /> --}}
+            </video>
+            <div class="course-content mt-3" style="width: 100%">
+              <div class="d-flex justify-content-between align-items-center mb-3">
+                <p class="category">Panduan Penyewaan Alat Lab. Untuk Dosen</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+          <div class="course-item">
+            {{-- <img src="assets/img/course-1.jpg" class="img-fluid" alt="..."> --}}
+            <video id="player6" playsinline controls>
+              <source src="{{ url('/public/assets/video/dosen_uji_lab.mp4') }}" type="video/mp4" />
+              {{-- <track kind="captions" label="English captions" src="MY_CAPTIONS.vtt" srclang="en" default /> --}}
+            </video>
+            <div class="course-content mt-3" style="width: 100%">
+              <div class="d-flex justify-content-between align-items-center mb-3">
+                <p class="category">Panduan Uji Laboratorium Untuk Dosen</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12" style="text-align: center;margin-bottom: 20px;">
+          <h3>Panduan Untuk Pengguna Umum</h3>
+        </div>
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+          <div class="course-item">
+            {{-- <img src="assets/img/course-1.jpg" class="img-fluid" alt="..."> --}}
+            <video id="player7" playsinline controls>
+              <source src="{{ url('/public/assets/video/umum_pinjam_lab.mp4') }}" type="video/mp4" />
+              {{-- <track kind="captions" label="English captions" src="MY_CAPTIONS.vtt" srclang="en" default /> --}}
+            </video>
+            <div class="course-content mt-3" style="width: 100%">
+              <div class="d-flex justify-content-between align-items-center mb-3">
+                <p class="category">Panduan Peminjaman Lab. Untuk Pengguna Umum</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+          <div class="course-item">
+            {{-- <img src="assets/img/course-1.jpg" class="img-fluid" alt="..."> --}}
+            <video id="player8" playsinline controls>
+              <source src="{{ url('/public/assets/video/umum_sewa_alat.mp4') }}" type="video/mp4" />
+              {{-- <track kind="captions" label="English captions" src="MY_CAPTIONS.vtt" srclang="en" default /> --}}
+            </video>
+            <div class="course-content mt-3" style="width: 100%">
+              <div class="d-flex justify-content-between align-items-center mb-3">
+                <p class="category">Panduan Penyewaan Alat Lab. Untuk Umum</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+          <div class="course-item">
+            {{-- <img src="assets/img/course-1.jpg" class="img-fluid" alt="..."> --}}
+            <video id="player9" playsinline controls>
+              <source src="{{ url('/public/assets/video/umum_uji_lab.mp4') }}" type="video/mp4" />
+              {{-- <track kind="captions" label="English captions" src="MY_CAPTIONS.vtt" srclang="en" default /> --}}
+            </video>
+            <div class="course-content mt-3" style="width: 100%">
+              <div class="d-flex justify-content-between align-items-center mb-3">
+                <p class="category">Panduan Uji Laboratorium Untuk Umum</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12" style="text-align: center;margin-bottom: 20px;">
+          <h3>Panduan Modul PDF Untuk Pengguna</h3>
+          Download Panduan PDF. <a href="{{ url('/public/assets/docs/Panduan_SIPLAB.pdf') }}"> <label class="label label-default">Panduan.pdf</label></a>
+          
         </div>
       </div>
     </div>
@@ -103,7 +180,7 @@ Lab management | Dashboard
 @endsection
 @push('css')
   <link rel="stylesheet" href="https://cdn.plyr.io/3.7.8/plyr.css">
-  <style>
+  {{-- <style>
     .plyr__video-wrapper,
     .plyr__audio-wrapper {
       width: 100%; /* Atur lebar sesuai kebutuhan, misalnya 100% */
@@ -111,12 +188,19 @@ Lab management | Dashboard
       max-width: 600px; /* Atur batas lebar maksimal */
       margin: 0 auto; /* Untuk memposisikan player di tengah */
     }
-  </style>
+  </style> --}}
 @endpush
 @push('scripts')
   <script src="https://cdn.plyr.io/3.7.8/plyr.js"></script>
   <script>
     const player1 = new Plyr('#player1');
     const player2 = new Plyr('#player2');
+    const player3 = new Plyr('#player3');
+    const player4 = new Plyr('#player4');
+    const player5 = new Plyr('#player5');
+    const player6 = new Plyr('#player6');
+    const player7 = new Plyr('#player7');
+    const player8 = new Plyr('#player8');
+    const player9 = new Plyr('#player9');
   </script>
 @endpush
